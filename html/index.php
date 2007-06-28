41fd4a4b7ce9abec36b49cc8fcc80758bbf1d30c
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-06-28 13:19:16 +0000

    Implemented php bug submitting
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@6734 594d385d-05f5-0310-b6e9-bd551577e9d8

--- html/index.php
+++ html/index.php
@@ -366,7 +366,7 @@ if (isset($_SESSION['errors'])){
   $smarty->assign("errors", $_SESSION['errors']);
 }
 if ($error_collector != ""){
-  $smarty->assign("php_errors", $error_collector."</div>");
+  $smarty->assign("php_errors", preg_replace("/%BUGBODY%/",$error_collector_mailto,$error_collector)."</div>");
 } else {
   $smarty->assign("php_errors", "");
 }
